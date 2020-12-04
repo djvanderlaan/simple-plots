6786d1be33e4023a6feff6f32855bbfefecb99a9
djvanderlaan <jan@knowledge-arcana.nl>
2020-12-04 12:15:23 +0100

    scatter

--- R/scatter.R
+++ R/scatter.R
@@ -1,53 +1,63 @@
 
-
-
-base_circle <- function(n = 25) {
-  phi <- seq(0, 2*pi, length.out = n+1) + 0.5*pi
-  phi <- head(phi, -1)
-  x <- cbind(cos(phi), sin(phi))
-  colnames(x) <- c("x", "y")
-  x
+scatter <- function(x, y, r, colour, border = colour, alpha = 0.2, 
+    add = FALSE) {
+  # When r is missing we just draw points
+  draw_bubble <- !(is.null(r) || missing(r))
+  if (!draw_bubble) r = 0
+  # This bit somewhat complicated: the radius should be given in the units that
+  # are on the x-axis. However, the units on the y-axis can be completely 
+  # different. To determin the x-range we can just do min(x-r) to max(x+r). For,
+  # y we need to scale the units to get a proper circle. The scaling factor is 
+  # calles asp below. Another complication is that our figure is probably not 
+  # square. The quareness of our figure is measured using asps (this is an 
+  # approximation). Below we try to estimate a good value for asp. The true 
+  # value of asp can only be calculated when teh figure is drawn however, we 
+  # need the limits first. 
+  s <- dev.size()
+  # Perhaps we could use par("pin") but a figure first needs to be open
+  asps <- s[1]/s[2]
+  asp <- 1
+  for (i in 1:3) {
+    xlim <- c(min(x-r), max(x+r))
+    ylim <- c(min(y-asp*asps*r), max(y+asp*asps*r))
+    asp <- diff(ylim)/diff(xlim)
+  }
+  # Open plot
+  if (!add) {
+    plot(x, y, xlim = xlim, ylim = ylim, type = 'n', 
+      bty = 'n', xaxt = 'n', yaxt = 'n', xlab ="", ylab = "")
+    # Todo allow user to specify properties of axes/grid etc.
+    axis(1, lwd = 0, lwd.ticks = 0)
+    axis(2, lwd = 0, lwd.ticks = 0, las = 1)
+    grid(lty = 3, col = "black")
+  }
+  # Draw
+  if (draw_bubble) {
+    fill <- adjustcolor(col, alpha.f = alpha)
+    symbols(x, y, circles = r, inches = FALSE, fg = border, 
+      bg = fill, add = TRUE)
+  } else {
+    points(x, y, pch = 20, col = col)
+  } 
 }
 
 
-n <- 20
-x <- runif(n, 0, 100)
-y <- runif(n, -1, 1)
-r <- runif(n, 0.1, 1)*5
-
-# xr <- diff(range(x))
-# yr <- diff(range(y))
-# asp <- yr/xr
-
-
-xmax <- max(x+r)
-xmin <- min(x-r)
-ymax <- max(y+r)
-ymin <- min(y-r)
-plot(x, y, xlim = c(xmin, xmax), ylim = c(ymin, ymax), pch = 20)
-
-
-w <- par("pin")[1]/diff(par("usr")[1:2])
-h <- par("pin")[2]/diff(par("usr")[3:4])
-asp <- w/h
-
-
-px <- mapply(function(x, y, r, asp) {
-    c <- base_circle(n=46) * r
-    c[,1] <- c[,1]+x
-    c[,2] <- c[,2]*asp+y
-    c <- rbind(c, c(NA, NA))
-    c[,1]
-  }, x, y, r, asp = asp)
-py <- mapply(function(x, y, r, asp) {
-    c <- base_circle(n=46) * r
-    c[,1] <- c[,1]+x
-    c[,2] <- c[,2]*asp+y
-    c <- rbind(c, c(NA, NA))
-    c[,2]
-  }, x, y, r, asp = asp)
-
-polygon(px, py, pch = '.', lty =1, col = "#00000010", border = "green")
 
 
 
+
+if (FALSE) {
+  pal <- hcl.colors(n = 10)
+  n <- 250
+  x <- runif(n, 0, 100)
+  y <- x * 0.1 *runif(n, -1, 1)
+  r <- 1+0.01*x*runif(n, 0.1, 1)*5
+  col <- pal[sample(1:10, n, replace = TRUE)]
+  
+  par(mar = c(4, 3, 2, 0)+0.2)
+  scatter(x, y, r, col)
+  mtext(side = 3, "Bla die bla score",  line = -2, adj = 0.02, outer = TRUE, 
+    cex = 1.2)
+  mtext(side = 1, "Aantal foobars", line = 2.5, adj=0.98, cex = 1.2)
+
+}
